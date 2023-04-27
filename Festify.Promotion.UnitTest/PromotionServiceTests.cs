8e0095ca150cf9cfea3371db4958727ecd295817
Rommel Sanchez Coronel <rommel.alan.sanchez@gmail.com>
2023-04-27 12:54:02 -0600

    Added first test for PromotionService

---
+++ Festify.Promotion.UnitTest/PromotionServiceTests.cs
@@ -0,0 +1,43 @@
+﻿using Festify.Promotion.Messages.Sales;
+using Festify.Promotion.Sales;
+using FluentAssertions;
+using MassTransit;
+using MassTransit.Testing;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Festify.Promotion.UnitTest
+{
+
+	public class PromotionServiceTests
+	{
+		[Fact]
+		public async Task WhenCustomerPurchasesItem_ThenPurchaseIsPublished()
+		{
+			InMemoryTestHarness harness = new InMemoryTestHarness();
+
+			await harness.Start();
+
+			//Arrange
+
+			IPublishEndpoint publishEndpoint = harness.Bus;
+			var producer = new PromotionService(publishEndpoint);
+
+			//Act
+			producer.PurchaseTicket();
+			await harness.InactivityTask;
+
+			//Assert
+			harness.Published.Select<OrderPlaced>()
+				.Count().Should().Be(1);
+
+
+			await harness.Stop();
+		}
+
+	}
+}
---
+++ Festify.Promotion/Sales/PromotionService.cs
@@ -0,0 +1,23 @@
+﻿using Festify.Promotion.Messages.Sales;
+using MassTransit;
+using MassTransit.Testing;
+using System;
+using System.Threading.Tasks;
+
+namespace Festify.Promotion.Sales
+{
+	public class PromotionService
+	{
+		private IPublishEndpoint publishEndpoint;
+
+		public PromotionService(IPublishEndpoint publishEndpoint)
+		{
+			this.publishEndpoint = publishEndpoint;
+		}
+
+		public async Task PurchaseTicket()
+		{
+			await publishEndpoint.Publish(new OrderPlaced());
+		}
+	}
+}
